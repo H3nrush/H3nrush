9a7d562178d4e2e824c59ae0d46595b016c78128
h3nrush <hassanmohammadi078@gmail.com>
2023-12-08 21:33:10 +0100

    h3nrush.com

---
+++ script.js
@@ -0,0 +1,10 @@
+function openMenu(){
+
+    if (document.getElementById("menu-container").style.display = "none"){
+        document.getElementById("menu-container").style.display = "block";
+    }
+}
+function closeMenu(){
+    document.getElementById("menu-container").style.display = "none"
+}
+
